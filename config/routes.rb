bacc1c115c5843af477206cc6f3a11ba1922529f
Stefano S <la.ss@katamail.com>
2014-07-22 01:18:42 +0200

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
 
 
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :sessions,   only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   #get 'users/new'
   #non più necessario dopo aver messo resources: users
 
