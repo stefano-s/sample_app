78419eaecf05906f65457af7b28a852381c5c7bd
Stefano S <la.ss@katamail.com>
2014-07-21 19:57:04 +0200

    Finish user edit, update, index, and destroy actions

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -16,10 +16,22 @@ describe "AuthenticationPages" do
         click_button "Sign in"
       end
 
+    #  it { should have_title(user.name) }
+     # it { should have_link('Profile',     href: user_path(user)) }
+      #it { should have_link('Sign out',    href: signout_path) }
+      #it { should_not have_link('Sign in', href: signin_path) }
+    
+
+
+### boh
+      let(:user) { FactoryGirl.create(:user) }
+      before { sign_in user }
       it { should have_title(user.name) }
       it { should have_link('Profile',     href: user_path(user)) }
+      it { should have_link('Settings',    href: edit_user_path(user)) }
       it { should have_link('Sign out',    href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
+
     end
 
 
